7530cdfca0144fb7687f8b340d93c16f76df401c
guizimo <17680262548@163.com>
2024-07-01 16:08:19 +0800

    feat(Prettier): 添加prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import uni from '@dcloudio/vite-plugin-uni';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
